13f19ab5087180cdc704d3c8300fb64084eb5aa2
aniket.dutta1998 <aniket.dutta@crystaltechlab.com>
2025-07-30 21:54:39 +0530

    dev: create new routes

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
     res.send("<h1> Server is running... </h1>");
 });
 
+app.get('/cicd', (req, res) => {
+    res.send("<h1> Succeesfully execute CI/CD pipeline using Terraform & Github Actions... </h1>");
+});
+
+
 app.listen(8080, () => console.log("Server running.."));
